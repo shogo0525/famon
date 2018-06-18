f8e1a2a8125a524110859bccbfb0bbc6e9ff983b
shogo_inamoto <shogo.ca2011+dev@gmail.com>
2018-06-18 22:58:17 +0900

    Add Items component

---
+++ resources/assets/js/components/Items.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<input type="number" placeholder="price" v-model="price">
+		<input type="date" v-model="date">
+		<button @click="addItem">登録</button>
+		<div v-for="item in items" :key="item.id">{{ item.price }} / {{ item.date }}</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'items',
+	data () {
+		return {
+			price: '',
+			date: '',
+			idForItem: 0,
+			items: []
+		}
+	},
+	methods: {
+		addItem() {
+			this.items.push({
+				id: this.idForItem,
+				price: this.price,
+				date: this.date,
+			})
+			this.price = ''
+			this.date = ''
+			this.idForItem++
+		}
+	}
+}
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
 </head>
 <body>
   <div id="app">
-    <example-component></example-component>
+    <items></items>
   </div>
   <script src="js/app.js"></script>
 </body>
