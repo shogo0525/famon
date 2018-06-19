3ec972d277d2805b170af10dcb0ec8d39792f4c8
shogo_inamoto <shogo.ca2011+dev@gmail.com>
2018-06-19 21:35:04 +0900

    Define alias for assets/js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -13,3 +13,12 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+
+mix.webpackConfig({
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, './resources/assets/js')
+    }
+  }
+})
