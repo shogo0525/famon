91bb8b638eac98012f7ceb2246d0afbf9a23ea9b
shogo_inamoto <shogo.ca2011+dev@gmail.com>
2018-06-20 13:56:56 +0900

    Install laravel/passport

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', 'AuthController@login');
+Route::post('/register', 'AuthController@register');
+Route::middleware('auth:api')->post('/logout', 'AuthController@logout');
+
 Route::get('/items', 'ItemController@index');
 Route::post('/items', 'ItemController@store');
 Route::patch('/items/{item}', 'ItemController@update');
