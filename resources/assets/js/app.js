2793bc4aa6e565a9d1938bd4bb8781793081fe82
shogo_inamoto <shogo.ca2011+dev@gmail.com>
2018-06-20 09:07:37 +0900

    Install vue-router

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,35 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './routes'
+Vue.use(VueRouter)
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-require('./bootstrap')
-
-window.Vue = require('vue')
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('items', require('./components/Items.vue'))
-
-// Webpack CSS import
 import 'onsenui/css/onsenui.css'
 import 'onsenui/css/onsen-css-components.css'
-
-// JS import
 import VueOnsen from 'vue-onsenui'
+Vue.use(VueOnsen)
 
 import { store } from './store/store'
 
-Vue.use(VueOnsen)
 
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
 
+import Master from '@/components/layouts/Master'
 const app = new Vue({
     el: '#app',
+    router,
     store,
+    components: { Master },
+    template: '<Master/>'
 })
---
+++ resources/assets/js/components/layouts/Master.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <ul class="nav">
+      <li><a href="">Home</a></li>
+      <li><a href="">Login</a></li>
+      <li><a href="">Logout</a></li>
+    </ul>
+    
+    <router-view></router-view>
+  </div>
+</template>
