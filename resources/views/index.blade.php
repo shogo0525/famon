edb86d552c85ab816da12105a632f865e416f6d5
shogo_inamoto <shogo.ca2011+dev@gmail.com>
2018-06-29 11:45:21 +0900

    Change title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="UTF-8">
-  <title>Hello Vue</title>
+  <title>famon</title>
   <link rel="stylesheet" href="css/app.css"/>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
